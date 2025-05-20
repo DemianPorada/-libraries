d431b8db4944af58d436b881b83c920818153bcd
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-05-20 20:53:29 +0300

    .

---
+++ main.js
@@ -0,0 +1,23 @@
+const coords = document.querySelector('.js-coords');
+let mouseMoveCounter = 0;
+
+function onMouseMove(event) {
+  mouseMoveCounter += 1;
+  coords.textContent = `
+    Кількість викликів onMouseMove: ${mouseMoveCounter},
+    X: ${event.clientX},
+    Y: ${event.clientY}
+  `;
+}
+
+window.addEventListener('mousemove', _.throttle(onMouseMove, 500));
+
+function onInputChange(event){
+    inputedElements += 1;
+    output.textContent =`
+     Кількість викликів onInputChange: ${inputedElements},
+    input: ${event.target.value}
+    `;
+}
+
+window.addEventListener('input', _.debounce(onInputChange, 500));
